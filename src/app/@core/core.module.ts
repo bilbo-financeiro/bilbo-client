88c06ef204f7a462aaeac842d7012a91a14554b7
Felipe Francischetto <felipefrancischetto@hotmail.com>
2019-03-31 13:28:08 -0300

    chore: added @firebase on project.

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 import { PagesModule } from '../pages/pages.module';
 import { LoginModule } from './../modules/security/login/login.module';
 import { NotFoundModule } from '../modules/not-found/not-found.module';
+
+import { environment } from './../../environments/environment';
 
 @NgModule({
   imports: [
@@ -11,11 +16,15 @@ import { NotFoundModule } from '../modules/not-found/not-found.module';
     PagesModule,
     LoginModule,
     NotFoundModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   exports: [
     PagesModule,
     LoginModule,
-    NotFoundModule
+    NotFoundModule,
+    AngularFireDatabaseModule,
+    AngularFireModule
   ]
 })
 export class CoreModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCauXulaWMnHvO_rKVtUppIqdNa2wTVI50',
+    authDomain: 'bildo-finc.firebaseapp.com',
+    databaseURL: 'https://bildo-finc.firebaseio.com',
+    projectId: 'bildo-finc',
+    storageBucket: '',
+    messagingSenderId: '6189561662'
+  }
 };
 
 /*
