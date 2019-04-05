51484d6dedab93932568152a3ad49e0b6e511b5c
Felipe Francischetto <felipefrancischetto@hotmail.com>
2019-04-04 21:45:37 -0300

    refactor: changes in route structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'pages', loadChildren: './pages/pages.module#PagesModule' },
-  { path: 'login', loadChildren: './modules/security/login/login.module#LoginModule' },
-  { path: 'signup', loadChildren: './modules/signup/signup.module#SignupModule'},
-  { path: '**', loadChildren: './modules/not-found/not-found.module#NotFoundModule' }
+  { path: '', redirectTo: 'pages/overview', pathMatch: 'full' },
+  { path: '**', loadChildren: './modules/not-found/not-found.module#NotFoundModule' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/pages.module.routing.ts
+++ src/app/pages/pages.module.routing.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PagesComponent } from './pages.component';
+import { AuthGuardService } from '../@core/guards/auth.guard';
 
 const ROUTE: Routes = [
   {
+    path: 'login',
+    loadChildren: '../modules/security/login/login.module#LoginModule'
+  },
+  {
+    path: 'signup',
+    loadChildren: '../modules/signup/signup.module#SignupModule'
+  },
+  {
     path: 'pages',
     component: PagesComponent,
-    canActivate: [ ],
+    canActivate: [AuthGuardService],
     children: [
+      { path: '', redirectTo: 'overview', pathMatch: 'full' },
       { path: 'overview', loadChildren: '../modules/overview/overview.module#OverviewModule' }
     ]
   }
@@ -16,6 +26,7 @@ const ROUTE: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(ROUTE)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuardService]
 })
 export class PagesRoutingModule { }
