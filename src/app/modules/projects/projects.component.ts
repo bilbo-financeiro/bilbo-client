634152ac5cd3844b38f347445fec8b61afd192e3
Felipe Francischetto <felipefrancischetto@hotmail.com>
2019-04-17 00:15:27 -0300

    feat: implemented exemple for wishes form.

--- src/app/modules/projects/projects.component.ts
+++ src/app/modules/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-projects',
@@ -7,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  public wishesForm: FormGroup;
 
-  ngOnInit() { }
+  public wishesList: any[] = [];
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.wishesForm = this.formBuilder.group({
+      name: '',
+      value: ''
+    });
+  }
+
+  insertWish(): void {
+    this.wishesList.push(this.wishesForm.value);
+  }
+
+  removeWish(index: number): void {
+    this.wishesList.splice(index, 1);
+  }
 
 }
